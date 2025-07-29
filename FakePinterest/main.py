4150d27f71947d072eb8516962c20b6b0b893fe7
maiki044 <mayquerodrigues04@gmail.com>
2025-07-29 17:16:46 -0300

    FakePiterest

--- FakePinterest/main.py
+++ FakePinterest/main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return "Welcome to the homepage! FakePinterest"
+    return render_template('homepage.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
+
