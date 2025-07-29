d89382aaff3fae78608f41284bc6bdfdd8f91ab8
maiki044 <mayquerodrigues04@gmail.com>
2025-07-29 17:19:01 -0300

    Update main.py

--- FakePinterest/main.py
+++ FakePinterest/main.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def homepage():
     return render_template('homepage.html')
 
+@app.route('/perfil')
+def perfil():
+    return render_template('perfil.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
 
