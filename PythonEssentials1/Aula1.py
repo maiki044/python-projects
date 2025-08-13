8c7087d249b4a57bf13be5bee89f7818970f4518
maiki044 <mayquerodrigues04@gmail.com>
2025-08-12 23:57:57 -0300

    Update Aula1.py

--- PythonEssentials1/Aula1.py
+++ PythonEssentials1/Aula1.py
@@ -1,2 +1,12 @@
-print(¡Hola Mundo!)
+try:
+    value = input("Digite um valor: ")
+    print(int(value)/len(value))
+except ValueError:
+    print("Entrada ruim...")
+except ZeroDivisionError:
+    print("Entrada muito ruim...")
+except TypeError:
+    print("Muito, muito ruim entrada...")
+except:
+    print("Booo!")
  
